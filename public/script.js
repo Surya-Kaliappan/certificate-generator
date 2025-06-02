9ab6f4d3fd74953f3daffb5e14d9c9d5c66cdb88
Surya-Kaliappan <kaliappansurya47@gmail.com>
2025-06-02 19:59:50 +1200

    Updated to Mobile Interaction

--- public/script.js
+++ public/script.js
@@ -33,7 +33,7 @@ const hamburgerMenu = document.getElementById('hamburgerMenu');
 const sidebar = document.querySelector('.sidebar');
 const mainSection = document.querySelector('.main');
 
-const api =  "http://192.168.56.235:3000";  // "https://cert-gen-app.onrender.com";
+const api = "https://cert-gen-app.onrender.com";
 
 // Initialize state variables
 let backgroundImage = null;
